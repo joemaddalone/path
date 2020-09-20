2553465d39c471f816cf3f095463bd07caefd9fb
Joe Maddalone <>
2020-09-20 14:21:07 -0500

    re-add demo without git

---
+++ demo/src/dag/dagNodes.js
@@ -0,0 +1,62 @@
+import Path from 'path';
+import dagSetup from './dagSetup';
+
+const dag = dagSetup();
+
+const square = (x, y, size, msg) => {
+  const p = new Path().moveTo(x, y).right(size).down(size).left(size).close();
+
+  const node = p.toElement({
+    class: 'square-node',
+  });
+  node.onclick = alert.bind(null, msg);
+  return node;
+};
+
+const circle = (x, y, radius, msg) => {
+  const p = new Path()
+    .M(x, y)
+    .m(-radius, 0)
+    .a(radius, radius, 0, 1, 0, radius * 2, 0)
+    .a(radius, radius, 0, 1, 0, -(radius * 2), 0);
+  const node = p.toElement({
+    class: 'circle-node',
+  });
+  node.onclick = alert.bind(null, msg);
+  return node;
+};
+
+const DagNodes = () => {
+  const createConnectors = () => {
+    const { edges } = dag;
+    const p = new Path();
+    edges.forEach(({ points, x, y }) => {
+      const m = points.shift();
+      p.M(m.x, m.y);
+      points.forEach((point) => p.L(point.x, point.y));
+    });
+    p.stroke('#222');
+
+    return p.toElement();
+  };
+
+  const squares = dag.nodes.slice(0, 5);
+  const circles = dag.nodes.slice(5);
+
+  const squareNodes = squares.map(({ x, y, width }, index) =>
+    square(x - width / 2, y - width / 2, width, `clicked node #${index}`),
+  );
+
+  const circleNodes = circles.map(({ x, y, width }, index) =>
+    circle(x, y, width / 2, `clicked node #${index}`),
+  );
+
+  return {
+    connectors: createConnectors(),
+    nodes: squareNodes.concat(circleNodes),
+    w: dag.graph.width,
+    h: dag.graph.height,
+  };
+};
+
+export default DagNodes;
---
+++ demo/src/macros/customMacro.js
@@ -0,0 +1,41 @@
+import Path from 'path';
+
+Path.macro('square', function (size, x, y) {
+  if (x && y) {
+    this.M(x, y); // only move if x & y are defined
+  }
+  this.right(size).down(size).left(size).up(size);
+  return this;
+});
+
+Path.macro('circle', function (radius, x, y) {
+  if (x && y) {
+    this.M(x, y); // only move if x & y are defined
+  }
+  this.m(-radius, 0)
+    .a(radius, radius, 0, 1, 0, -(radius * 2), 0)
+    .a(radius, radius, 0, 1, 0, radius * 2, 0);
+  return this;
+});
+
+Path.macro('triangle', function (w, h, x, y) {
+  this.M(x, y) // only move if x & y are defined
+    .l(-w / 2, h)
+    .right(w)
+    .L(x, y);
+  return this;
+});
+
+const customMacros = () => {
+  const p = new Path();
+  p.square(30, 25, 25)
+    .square(50, 60, 15)
+    .circle(15, 160, 40)
+    .circle(25, 225, 40)
+    .triangle(25, 25, 220, 30)
+    .triangle(65, 50, 265, 15)
+    .triangle(25, 25, 310, 30);
+  return p.toElement({ stroke: '#222' });
+};
+
+export default customMacros;
