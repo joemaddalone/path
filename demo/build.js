76a504542a4ef04e22626cbc7a03188df088272a
Joe Maddalone <joe.maddalone@gmail.com>
2025-08-23 12:01:14 -0500

    demo builder

--- demo/build.js
+++ demo/build.js
@@ -7,33 +7,11 @@ require("esbuild")
     define: {
       "process.env.NODE_ENV": '"development"',
 	},
-    watch: {
-      onRebuild(error, result) {
-        if (error) console.error("watch build failed:", error);
-        else console.error("watch build succeeded:", result);
-      },
-    },
+    // watch: {
+    //   onRebuild(error, result) {
+    //     if (error) console.error("watch build failed:", error);
+    //     else console.error("watch build succeeded:", result);
+    //   },
+    // },
   })
   .catch(() => process.exit(1));
-
-// const esbuild = require("esbuild");
-
-// // workaround for https://github.com/evanw/esbuild/issues/730
-// esbuild.startService().then((service) =>
-//   service.build({
-//     entryPoints: ["src/app.jsx"],
-//     bundle: true,
-//     platform: "browser",
-//     minify: true,
-//     outfile: "public/index.js",
-//     define: {
-//       "process.env.NODE_ENV": '"production"',
-//     },
-//     watch: {
-//       onRebuild(error, result) {
-//         if (error) console.error("watch build failed:", error);
-//         else console.error("watch build succeeded:", result);
-//       },
-//     },
-//   })
-// );
